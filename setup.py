ad81cb89182b21344662820e01d88a45a0763119
David Seddon <david@seddonym.me>
2014-10-06 16:45:35 +0100

    Bumped the version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 
 setup(
     name='bobsleigh',
-    version='0.1.3',
+    version='0.1.4',
     url='http://github.com/seddonym/bobsleigh/',
     author='David Seddon',
     author_email='david@seddonym.me',
